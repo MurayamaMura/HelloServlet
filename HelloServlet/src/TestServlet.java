ce33f66db1fffd3a490f55c6140c240b090c0b13
MurayamaMura <murayama.89.bump.jump.1@gmail.com>
2018-08-03 14:00:44 +0900

    0803

--- HelloServlet/src/TestServlet.java
+++ HelloServlet/src/TestServlet.java
@@ -8,29 +8,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
-public TestServlet() {
-super();
-}
-protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
-ServletException, IOException {
-request.setCharacterEncoding("UTF-8");
-response.setContentType("text/html; charset=UTF-8");
-String username=request.getParameter("username");
-String password=request.getParameter("password");
-System.out.println(username);
-System.out.println(password);
-PrintWriter out=response.getWriter();
-out.println("<html><head></head><body><br>"+username+"<br>"+password+"</body></html>");
-}
-protected void doPost(HttpServletRequest request, HttpServletResponse response) throws
-ServletException, IOException {
-request.setCharacterEncoding("UTF-8");
-response.setContentType("text/html; charset=UTF-8");
-String username=request.getParameter("username");
-String password=request.getParameter("password");
-System.out.println(username);
-System.out.println(password);
-PrintWriter out=response.getWriter();
-out.println("<html><head></head><body><br>"+username+"<br>"+password+"</body></html>");
-}
+	public TestServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
+		ServletException, IOException {
+			request.setCharacterEncoding("UTF-8");
+			response.setContentType("text/html; charset=UTF-8");
+			String username=request.getParameter("username");
+			String password=request.getParameter("password");
+			System.out.println(username);
+			System.out.println(password);
+			PrintWriter out=response.getWriter();
+			out.println("<html><head></head><body><br>"+username+"<br>"+password+"</body></html>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws
+		ServletException, IOException {
+			request.setCharacterEncoding("UTF-8");
+			response.setContentType("text/html; charset=UTF-8");
+			String username=request.getParameter("username");
+			String password=request.getParameter("password");
+			System.out.println(username);
+			System.out.println(password);
+			PrintWriter out=response.getWriter();
+			out.println("<html><head></head><body><br>"+username+"<br>"+password+"</body></html>");
+	}
 }
